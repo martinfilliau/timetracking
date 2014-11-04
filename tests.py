49da3f9decd27a6b3e61b807226c71b398c2b9ac
Martin Filliau <martin@filliau.com>
2014-11-04 12:13:55 +0000

    Updated tests so that it runs with python 3

--- tests.py
+++ tests.py
@@ -13,29 +13,29 @@ class TimetrackingTest(unittest.TestCase):
 class DateTimeHelpersTest(unittest.TestCase):
 
     def test_within_true(self):
-        from_date = datetime(2014, 01, 01)
-        to_date = datetime(2014, 06, 01)
-        test_date = datetime(2014, 03, 01)
+        from_date = datetime(2014, 1, 1)
+        to_date = datetime(2014, 6, 1)
+        test_date = datetime(2014, 3, 1)
         value = within(test_date, from_date, to_date)
         self.assertEquals(value, True)
 
     def test_within_false(self):
-        from_date = datetime(2014, 01, 01)
-        to_date = datetime(2014, 06, 01)
-        test_date = datetime(2014, 07, 01)
+        from_date = datetime(2014, 1, 1)
+        to_date = datetime(2014, 6, 1)
+        test_date = datetime(2014, 7, 1)
         value = within(test_date, from_date, to_date)
         self.assertEquals(value, False)
 
     def test_within_edge(self):
-        from_date = datetime(2014, 01, 01)
-        to_date = datetime(2014, 06, 01)
-        test_date = datetime(2014, 06, 01)
+        from_date = datetime(2014, 1, 1)
+        to_date = datetime(2014, 6, 1)
+        test_date = datetime(2014, 6, 1)
         value = within(test_date, from_date, to_date)
         self.assertEquals(value, False)
 
     def test_calculate_time(self):
-        start = datetime(2014, 01, 01)
-        end = datetime(2014, 02, 01)
+        start = datetime(2014, 1, 1)
+        end = datetime(2014, 2, 1)
         delta = timedelta(days=31)
         value = calculate_time(start, end)
         self.assertEquals(value, delta)
